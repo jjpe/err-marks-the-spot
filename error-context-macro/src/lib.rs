a400d7d7c373c9d202553affc8180174efca79b7
Joey Ezechiëls <joey.ezechiels@gmail.com>
2025-04-27 00:05:06 +0200

    Generate Location and Backtrace fields for #[contextual_error] types

--- error-context-macro/src/lib.rs
+++ error-context-macro/src/lib.rs
@@ -1,20 +1,22 @@
 //!
 
 use proc_macro::{TokenStream, Span};
-use proc_macro2::{TokenStream as TokenStream2, Span as Span2};
+use proc_macro2::{Ident as Ident2, TokenStream as TokenStream2, Span as Span2};
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{
+    parse_macro_input,
+    Token,
+    punctuated::Punctuated,
+    token::{And, Brace, Colon, Gt, Lt, Paren, PathSep, Pub},
+    AngleBracketedGenericArguments, Data, DataEnum, DataStruct, DeriveInput,
+    Field, FieldMutability, GenericArgument, Fields, FieldsNamed, FieldsUnnamed,
+    Lifetime, Path, PathArguments, PathSegment, Type, TypePath, TypeReference,
+    Visibility,
+};
 
 
 #[proc_macro_attribute]
-pub fn contextual_error(attr: TokenStream, item: TokenStream) -> TokenStream {
-    // let DeriveInput {
-    //     attrs: attr_attrs,
-    //     vis: attr_vis,
-    //     ident: attr_ident,
-    //     generics: attr_generics,
-    //     data: attr_data
-    // } = parse_macro_input!(attr as DeriveInput);
+pub fn contextual_error(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let DeriveInput {
         attrs: item_attrs,
         vis: item_vis,
@@ -23,13 +25,181 @@ pub fn contextual_error(attr: TokenStream, item: TokenStream) -> TokenStream {
         data: item_data
     } = parse_macro_input!(item as DeriveInput);
 
-    println!("attr: {attr:#?}");
-    println!("attrs: {item_attrs:#?}");
-
-
-
-    let output: TokenStream2 = quote! {
-        // TODO
+    let output_data = match item_data {
+        Data::Union(_) => panic!("[contextual_error] Unions are not supported"),
+        Data::Enum(e) => {
+            todo!("Data::Enum");
+            // Data::Enum(DataEnum {
+            //     enum_token: e.enum_token,
+            //     brace_token: e.brace_token,
+            //     variants: (),
+            // })
+        },
+        Data::Struct(s) => match s.fields {
+            Fields::Named(n) => Data::Struct(DataStruct {
+                struct_token: s.struct_token,
+                fields: Fields::Named(FieldsNamed {
+                    brace_token: Brace(Span2::call_site()),
+                    named: std::iter::empty()
+                        .chain(n.named.into_iter()) // user-defined fields
+                        .chain(
+                            vec![
+                                named_location_field(),
+                                named_backtrace_field(),
+                            ].into_iter()
+                        )
+                        .collect(),
+                }),
+                semi_token: s.semi_token,
+            }),
+            Fields::Unit => Data::Struct(DataStruct {
+                struct_token: s.struct_token,
+                fields: Fields::Named(FieldsNamed {
+                    brace_token: Brace(Span2::call_site()),
+                    named: vec![
+                        named_location_field(),
+                        named_backtrace_field(),
+                    ].into_iter().collect(),
+                }),
+                semi_token: s.semi_token,
+            }),
+            Fields::Unnamed(u) => Data::Struct(DataStruct {
+                struct_token: s.struct_token,
+                fields: Fields::Unnamed(FieldsUnnamed {
+                    paren_token: Paren(Span2::call_site()),
+                    unnamed: std::iter::empty()
+                        .chain(u.unnamed.into_iter()) // user-defined fields
+                        .chain(vec![
+                            unnamed_location_field(),
+                            unnamed_backtrace_field(),
+                        ]
+                               .into_iter())
+                        .collect(),
+                }),
+                semi_token: s.semi_token,
+            }),
+        },
     };
-    TokenStream::from(output)
+
+    let output = DeriveInput {
+        attrs: item_attrs,
+        vis: item_vis,
+        ident: item_ident,
+        generics: item_generics,
+        data: output_data
+    };
+    TokenStream::from(quote! {
+        #output
+    })
+}
+
+// pub location: &'static std::panic::Location<'static>
+fn named_location_field() -> Field {
+    Field {
+        attrs: vec![/*none*/],
+        vis: Visibility::Public(Pub(Span2::call_site())),
+        mutability: FieldMutability::None,
+        ident: Some(Ident2::new("location", Span2::call_site())),
+        colon_token: Some(Colon(Span2::call_site())),
+        ty: location_type(),
+    }
+}
+
+// pub backtrace: std::backtrace::Backtrace
+fn named_backtrace_field() -> Field {
+    Field {
+        attrs: vec![/*none*/],
+        vis: Visibility::Public(Pub(Span2::call_site())),
+        mutability: FieldMutability::None,
+        ident: Some(Ident2::new("backtrace", Span2::call_site())),
+        colon_token: Some(Colon(Span2::call_site())),
+        ty: backtrace_type(),
+    }
+}
+
+// pub &'static std::panic::Location<'static>
+fn unnamed_location_field() -> Field {
+    Field {
+        attrs: vec![/*none*/],
+        vis: Visibility::Public(Pub(Span2::call_site())),
+        mutability: FieldMutability::None,
+        ident: None,
+        colon_token: None,
+        ty: location_type(),
+    }
+}
+
+// pub std::backtrace::Backtrace
+fn unnamed_backtrace_field() -> Field {
+    Field {
+        attrs: vec![/*none*/],
+        vis: Visibility::Public(Pub(Span2::call_site())),
+        mutability: FieldMutability::None,
+        ident: None,
+        colon_token: None,
+        ty: backtrace_type(),
+    }
+}
+
+fn location_type() -> Type {
+    Type::Reference(TypeReference {
+        and_token: And(Span2::call_site()),
+        lifetime: Some(Lifetime::new("'static", Span2::call_site())),
+        mutability: None,
+        elem: Box::new(Type::Path(TypePath {
+            qself: None,
+            path: Path {
+                leading_colon: None,
+                segments: vec![
+                    PathSegment {
+                        ident: Ident2::new("std", Span2::call_site()),
+                        arguments: PathArguments::None,
+                    },
+                    PathSegment {
+                        ident: Ident2::new("panic", Span2::call_site()),
+                        arguments: PathArguments::None,
+                    },
+                    PathSegment {
+                        ident: Ident2::new("Location", Span2::call_site()),
+                        arguments: PathArguments::AngleBracketed(
+                            AngleBracketedGenericArguments {
+                                colon2_token: Some(PathSep::default()),
+                                lt_token: Lt(Span2::call_site()),
+                                args: vec![
+                                    GenericArgument::Lifetime(Lifetime::new(
+                                        "'static",
+                                        Span2::call_site()
+                                    ))
+                                ].into_iter().collect(),
+                                gt_token: Gt(Span2::call_site())
+                            }
+                        )
+                    },
+                ].into_iter().collect()
+            },
+        }))
+    })
+}
+
+fn backtrace_type() -> Type {
+    Type::Path(TypePath {
+        qself: None,
+        path: Path {
+            leading_colon: None,
+            segments: vec![
+                PathSegment {
+                    ident: Ident2::new("std", Span2::call_site()),
+                    arguments: PathArguments::None,
+                },
+                PathSegment {
+                    ident: Ident2::new("backtrace", Span2::call_site()),
+                    arguments: PathArguments::None,
+                },
+                PathSegment {
+                    ident: Ident2::new("Backtrace", Span2::call_site()),
+                    arguments: PathArguments::None
+                },
+            ].into_iter().collect()
+        },
+    })
 }
--- error-context-test/src/lib.rs
+++ error-context-test/src/lib.rs
@@ -3,17 +3,26 @@
 use error_context_macro::contextual_error;
 
 #[contextual_error]
-pub struct FooError {
-    pub field0: usize,
-}
+#[derive(Debug)]
+pub struct FooError(usize);
 
+// TODO: Generate a ctor for the type on which `#[contextual_error]` is used,
+//       because otherwise the Location and Backtrace need to be provided
+//       manually, which is super annoying.
+//       For structs this can be a single ctor called `new`, while for enums it
+//       should be a single ctor per enum variant carrying the variant's name.
 
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
     fn it_works() {
-        println!("blah blah blah");
+        let err = FooError(
+            0,
+            std::panic::Location::caller(),
+            std::backtrace::Backtrace::capture(),
+        );
     }
 }
