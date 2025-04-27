632054e09978cbcd65eb10051419510c8bc99c5c
Joey Ezechiëls <joey.ezechiels@gmail.com>
2025-04-27 15:00:20 +0200

    Add simple tests for {named,tuple,unit} structs and enum variants

--- error-context-test/src/lib.rs
+++ error-context-test/src/lib.rs
@@ -1,17 +1,29 @@
 //!
+#![allow(unused)]
 
 use error_context_macro::contextual_error;
 
 #[contextual_error]
 #[derive(Debug)]
-pub struct FooError(usize);
+pub struct TupleStructError(usize);
 
-// TODO: Generate a ctor for the type on which `#[contextual_error]` is used,
-//       because otherwise the Location and Backtrace need to be provided
-//       manually, which is super annoying.
-//       For structs this can be a single ctor called `new`, while for enums it
-//       should be a single ctor per enum variant carrying the variant's name.
+#[contextual_error]
+#[derive(Debug)]
+pub struct NamedStructError {
+    f0: String,
+}
 
+#[contextual_error]
+#[derive(Debug)]
+pub struct UnitStructError;
+
+#[contextual_error]
+#[derive(Debug)]
+pub enum EnumError {
+    Tuple(usize),
+    Named { f0: usize, },
+    Unit,
+}
 
 #[cfg(test)]
 mod tests {
@@ -19,10 +31,34 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let err = FooError(
+        let tuple_strct_error = TupleStructError(
             0,
             std::panic::Location::caller(),
             std::backtrace::Backtrace::capture(),
         );
+        let named_strct_error = NamedStructError {
+            f0: "foo".to_string(),
+            location: std::panic::Location::caller(),
+            backtrace: std::backtrace::Backtrace::capture(),
+        };
+        let unit_strct_error = UnitStructError {
+            location: std::panic::Location::caller(),
+            backtrace: std::backtrace::Backtrace::capture(),
+        };
+
+        let tuple_enum_error = EnumError::Tuple(
+            300,
+            std::panic::Location::caller(),
+            std::backtrace::Backtrace::capture(),
+        );
+        let named_enum_error = EnumError::Named {
+            f0: 42,
+            location: std::panic::Location::caller(),
+            backtrace: std::backtrace::Backtrace::capture(),
+        };
+        let unit_enum_error = EnumError::Unit {
+            location: std::panic::Location::caller(),
+            backtrace: std::backtrace::Backtrace::capture(),
+        };
     }
 }
